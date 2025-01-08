8e71751cbb130e79aef37599d9910fa8beac91d6
Devesh Kumar Arya <devesharya@acies.holdings>
2025-01-08 10:59:17 +0530

    updated the if-else block to put dynamic condition_value

--- Code/reporting_framework.py
+++ Code/reporting_framework.py
@@ -858,10 +858,17 @@ for idx, scenario in grouped_scenarios.iterrows():
                             logging.warning(f"Invalid tenor_unit: {tenor_unit}")
                 
                         if new_date is not None:
-                            days_difference = (new_date - reporting_date).days
-                            calculated_time_to_maturity = days_difference / 365
-                            condition['condition_value'] = calculated_time_to_maturity
-                            logging.warning(f"Calculated time to maturity: {calculated_time_to_maturity}")
+                            if condition['condition_datatype'] == 'Date':
+                                condition['condition_value'] = new_date
+                                logging.warning(f"Calculated date: {new_date}")
+                            elif condition['condition_datatype'] == 'Numeric':
+                                days_difference = (new_date - reporting_date).days
+                                calculated_time_to_maturity = days_difference / 365
+                                condition['condition_value'] = calculated_time_to_maturity
+                                logging.warning(f"Calculated time to maturity: {calculated_time_to_maturity}")
+                            else:
+                                condition['condition_value'] = None
+                                logging.warning("Failed to calculate condition value due to invalid condition dataype.")    
                         else:
                             condition['condition_value'] = None
                             logging.warning("Failed to calculate new_date due to invalid tenor_unit.")
